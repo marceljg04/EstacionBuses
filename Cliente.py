6f2be1eb3b304e45cc0dfc76b6e8df60ce4b53a4
Marcel Jimenez <marceljg04@gmail.com>
2025-09-17 12:58:39 +0100

    Cliente (faltan getters y setters)

--- Cliente.py
+++ Cliente.py
@@ -1,2 +1,18 @@
+from Billete import Billete
+
 class Clientes:
+    def __init__(self, nombre, apellido):
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__billete =  None
     
+    def asignarBillete(self, billete):
+        if self.billete is not None:
+            raise ValueError(f"El cliente {self.nombre} ya tiene un billete asignado.")
+        self.billete = billete
+
+    def devolverBillete(self, billete):
+        if self.billete is None:
+            raise ValueError(f"El cliente {self.nombre} no tiene billete para devolver.")
+        self.billete = None
+
