97d3fb601ec3b86c049a28604ddd17ff6d911a66
Marcel Jimenez <marceljg04@gmail.com>
2025-09-17 16:22:19 +0200

    Clase Billete y Cliente hechas (Clase Bus TO-DO)

--- Billete.py
+++ Billete.py
@@ -1,11 +1,35 @@
 """
-billetes una clase con los atributos nombre, apellido, cantidad de billetes
-funciones: getters, setters
+Los billetes tienen un identificador, la informacion del cliente y la del bus
 """
 
-class Billetes:
-    
-    def __init__(self, nombre, apellido, cantidad):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.cantidad = cantidad
+class Billete:
+    def __init__(self, id_billete, cliente, bus):
+        self.__id_billete = id_billete
+        self.__cliente = cliente
+        self.__bus = bus
+
+    # Getters
+    def getIdBillete(self):
+        return self.__id_billete
+
+    def getCliente(self):
+        return self.__cliente
+
+    def getBus(self):
+        return self.__bus
+
+
+    # Setters
+    def setIdBillete(self, id_billete):
+        self.__id_billete = id_billete
+
+    def setCliente(self, cliente):
+        self.__cliente = cliente
+
+    def setBus(self, bus):
+        self.__bus = bus
+
+
+    # Metodos
+    def __str__(self):
+        return f"Billete #{self.__id_billete} de {self.__cliente.nombre} {self.__cliente.apellido}"
--- Cliente.py
+++ Cliente.py
@@ -1,3 +1,7 @@
+"""
+Los clientes tienen un objeto billete en sus atributos, este se puede asignar o devolver
+"""
+
 from Billete import Billete
 
 class Clientes:
@@ -5,14 +9,35 @@ class Clientes:
         self.__nombre = nombre
         self.__apellido = apellido
         self.__billete =  None
-    
+   
+    #  Getters 
+    def getNombre(self):
+        return self.__nombre
+
+    def getApellido(self):
+        return self.__apellido
+
+    def getBillete(self):
+        return self.__billete
+
+    #  Setters 
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+
+    def setApellido(self, apellido):
+        self.__apellido = apellido
+
+
+    def setBillete(self, billete):
+        self.__billete = billete
+
+    #  Metodos
     def asignarBillete(self, billete):
-        if self.billete is not None:
-            raise ValueError(f"El cliente {self.nombre} ya tiene un billete asignado.")
-        self.billete = billete
+        if self.__billete is not None:
+            raise ValueError(f"El cliente {self.__nombre} ya tiene un billete asignado.")
+        self.__billete = billete
 
-    def devolverBillete(self, billete):
-        if self.billete is None:
-            raise ValueError(f"El cliente {self.nombre} no tiene billete para devolver.")
-        self.billete = None
-
+    def devolverBillete(self):
+        if self.__billete is None:
+            raise ValueError(f"El cliente {self.__nombre} no tiene billete para devolver.")
+        self.__billete = None
